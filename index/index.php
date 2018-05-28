b5d8367a93a909811d8d15f14f71b4e89392b0df
alexml12 <alexml_12@hotmail.com>
2018-05-28 14:53:36 -0500

    primer commit
    Creación del index

---
+++ index/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>prueba de página 1 </title>
+</head>
+<body>
+ <h1>los integrantes del grupo son:</h1>
+ <h2>1: MARILIN CAROLINA HUAISARA BAUTISTA</h2>
+ <h2>2: ALEXANDER VALDEZ FLORES</h2>
+ <h2>3: JHONNY MARCATINCO ESPEJO</h2>
+ <h2>4: SERGIO PFUÑO</h2>
+</body>
+</html>
